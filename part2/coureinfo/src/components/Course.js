3ab7f7815d6ea6a0db7e13f3a2b50cf6bc36dddf
c4tghs <34139848+c4tghs@users.noreply.github.com>
2022-01-21 17:08:06 +0100

    Exercise 2.5 separate module complete

--- part2/coureinfo/src/components/Course.js
+++ part2/coureinfo/src/components/Course.js
@@ -1,7 +1,35 @@
 import React from "react";
-import Header from "./Header"
-import Content from "./Content";
-import Total from "./Total"
+
+
+const Header = ({title}) => {
+  return (
+    <h2>{title}</h2>
+  )
+}
+
+const Content = ({course}) => {
+  return (
+    <div>
+      {course.map((part) => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+
+const Total = ({parts}) => {
+  const total = parts.reduce((previousVal,currentVal) => previousVal + currentVal.exercises,0) 
+  
+  return(
+      <strong>total of {total} exercises</strong>
+  )
+}
+
+const Part = ({part}) => {
+  return(
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
 
 const Course =({course}) => {
     return (
