4011646ce929a938580e5519f54c310248d28626
c4tghs <34139848+c4tghs@users.noreply.github.com>
2022-01-27 13:44:03 +0100

    Part 2e notes

--- part2/part2-notes/src/App.js
+++ part2/part2-notes/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
 const App = () => {  
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService      
@@ -43,10 +46,12 @@ const App = () => {
       setNotes(notes.map(note => note.id !== id ? note : returnedNote))      
     })
     .catch(error => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
+      setErrorMessage(
+        `Note '${note.content}' was already removed from server`
       )
-      setNotes(notes.filter(n => n.id !== id))
+      setTimeout(() => {
+        setErrorMessage(null)
+      },5000)
     })
   }
 
@@ -60,6 +65,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <div>
         <button onClick={() => setShowAll(!showAll)}> 
           show {showAll ? 'important' : 'all'}
@@ -74,6 +80,8 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange}/>        
         <button type="submit">save</button>      
       </form>  
+
+      <Footer />
     </div>
   )
 }
