24c78d62b60cdb6a02fdc9e871d0adf870a1cacd
c4tghs <34139848+c4tghs@users.noreply.github.com>
2022-02-05 13:36:25 +0100

    Exercise 3.12 command-line database

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+if (process.argv.length > 5){
+    console.log('Please provide maximum of 3 arguments: node mongo.js <password> <name> <number>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const phoneNumber = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.iwch7.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: name,
+  number: phoneNumber
+})
+
+if(process.argv.length > 3){
+    person.save().then(result => {
+        console.log(`added ${name} number ${phoneNumber} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+else{
+    Person.find({}).then(result => {
+        console.log("phonebook:");
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+        })
+        mongoose.connection.close()
+    })
+}
