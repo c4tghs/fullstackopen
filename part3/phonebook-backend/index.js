ad6fddca80f143a32eb287e4c0a11a725059a54a
c4tghs <34139848+c4tghs@users.noreply.github.com>
2022-02-05 15:02:15 +0100

    Exercises 3.13-3.14 phonebook database

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,40 +1,19 @@
-const { response } = require('express')
+require('dotenv').config
+console.log(require("dotenv").config())
 const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
-
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
+const Person = require('./models/persons')
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id',(request,response) => {
@@ -61,19 +40,19 @@ app.post('/api/persons', (request,response) => {
             error:'content missing'
         })
     }
-    else if(persons.find(person => person.name === body.name)){
+    /*else if(persons.find(person => person.name === body.name)){
         return response.status(409).json({
             error:'name must be unique'
         })
-    }
+    }*/
     else{
-        const person = {
-            id: Math.floor(Math.random()*1000),
+        const person = new Person({
             name:body.name,
             number:body.number
-        }
-        persons = persons.concat(person)
-        response.json(person)
+        })
+        person.save().then(savedPerson => {
+            response.json(savedPerson)
+        })
     }
 })
 
@@ -88,7 +67,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 }
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
