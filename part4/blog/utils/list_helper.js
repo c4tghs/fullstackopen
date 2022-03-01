51f9c1450ddbb333bac0e6d297ccc8ebd8815bea
c4tghs <34139848+c4tghs@users.noreply.github.com>
2022-03-01 16:54:04 +0100

    Helper functions and unit test step 2

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+    let likes = blogs.reduce((prev, curr) => {
+      return prev + curr.likes
+    }, 0)
+
+    return likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
